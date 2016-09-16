fb3dd5ca6a5bda697dc636d36f2fcd57a9105ede
Boris shishov <borisshishov@gmail.com>
2016-09-16 22:32:03 +0300

    Markdown editor

--- main/apps.py
+++ main/apps.py
@@ -7,4 +7,8 @@ class MainConfig(AppConfig):
 
 
 # array of editors for different types of items
-EDITORS = [editors.CodeEditor(), editors.DirectoryEditor(), editors.FileEditor(), editors.UniversalEditor()]
+EDITORS = [editors.CodeEditor(),
+           editors.MarkdownEditor(),
+           editors.DirectoryEditor(),
+           editors.FileEditor(),
+           editors.UniversalEditor()]
